04c1ba379c252892f1a409f8f463dab9ecb7c054
ZenithNUC <lizengzhi1998@foxmail.com>
2021-01-14 14:47:59 +0800

    feat:add model file

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module com.sso.zenith
 
 go 1.14
 
-require gopkg.in/yaml.v2 v2.4.0 // indirect
+require (
+	github.com/jinzhu/gorm v1.9.16 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
---
+++ model/model.go
@@ -0,0 +1,36 @@
+package model
+
+import (
+	"com.sso.zenith/config"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"log"
+	"time"
+)
+
+var db *gorm.DB
+
+func Setup(){
+	var err error
+	cfg := config.Get()
+	db,err = gorm.Open(cfg.Db.Default.Type,fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local",cfg.Db.Default.User,cfg.Db.Default.Password,cfg.Db.Default.Host,cfg.Db.Default.DbName))
+	if err != nil {
+		log.Println(err)
+	}
+	db.SingularTable(true)
+	db.LogMode(true)
+	db.DB().SetMaxIdleConns(10)
+	db.DB().SetMaxOpenConns(100)
+}
+
+func closeDB(){
+	defer db.Close()
+}
+
+type Model struct {
+	ID        uint `gorm:"primary_key" json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at"`
+}
